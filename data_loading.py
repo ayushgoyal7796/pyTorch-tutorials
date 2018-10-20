a2f00541b0f2c388ce98158d8d462f4c9342988c
ayushgoyal7796 <ayushgoyal7796@gmail.com>
2018-10-20 19:16:23 +0530

    Adding data_loading.py

--- data_loading.py
+++ data_loading.py
@@ -342,3 +342,48 @@ for i_batch, sample_batched in enumerate(dataloader):
         plt.show()
         break
 
+
+# In[16]:
+
+
+# Afterword : Torchvision
+# we have seen how to write and use datasets, transforms, and dataloader.
+# torchvision package provides some common datasets and transforms. You may not even have to write custom classes.
+
+
+# In[17]:
+
+
+# One of the more genric datasets is 'ImageFolder'. It assumes that images are orgaminzed in following way:
+    #root/ants/xxx.png
+    #root/ants/xxy.jpeg
+    #root/ants/xxz.png
+    #.
+    #.
+    #.
+    #root/bees/123.jpg
+    #root/bees/nsdf3.png
+    #root/bees/asd932_.png
+# where ‘ants’, ‘bees’ etc. are class labels
+
+
+# In[18]:
+
+
+#  Similarly generic transforms which operate on PIL.Image like RandomHorizontalFlip, Scale, are also available. You can use these to write a dataloader like this:
+import torch
+from torchvision import transforms, datasets
+
+data_transform = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+hymenoptera_dataset = datasets.ImageFolder(root='hymenoptera_data/train',
+                                           transform=data_transform)
+dataset_loader = torch.utils.data.DataLoader(hymenoptera_dataset,
+                                             batch_size=4, shuffle=True,
+                                             num_workers=4)
+
